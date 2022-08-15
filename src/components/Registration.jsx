d0d578d5266d3945726ff7511855c80721168c3c
Astemir Bekov <artisticheski@gmail.com>
2022-08-15 17:48:03 +0300

    fixed validation form

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,17 +1,105 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import "../styles/main.css";
 import '../styles/Registration.css'
 
 const Registration = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [firstNameDirty, setFirstNameDirty] = useState(false);
+  const [lastNameDirty, setLastNameDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [firstNameError, setFirstNameError] = useState("This field can't be empty");
+  const [lastNameError, setLastNameError] = useState("This field can't be empty");
+  const [passwordError, setPasswordError] = useState("This field can't be empty");
+  const [emailError, setEmailError] = useState("This field can't be empty");
+
+  useEffect(() => {
+
+  }, [])
+
+  const firstNameHandler = (event) => {
+    setFirstName(event.target.value);
+    if (event.target.value.length >= 2) {
+      setFirstNameError("");
+    } else {
+      setFirstNameError("invalid name");
+    }
+  }
+
+  const lastNameHandler = (event) => {
+    setLastName(event.target.value);
+    if (event.target.value.length >= 2) {
+      setLastNameError("");
+    } else {
+      setLastNameError("invalid last name");
+    }
+  }
+
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!re.test(String(event.target.value).toLowerCase())) {
+      setEmailError("Invalid email");
+    } else {
+      setEmailError("");
+    }
+  }
+
+  const passwordHandler = (event) => {
+    setPassword(event.target.value);
+    if (event.target.value.length < 3) {
+      setPasswordError("you have too week password");
+    } else {
+      setPasswordError("");
+    }
+  }
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "firstName":
+        setFirstNameDirty(true);
+        break;
+      case "lastName":
+        setLastNameDirty(true);
+        break;
+      case "email":
+        setEmailDirty(true);
+        break;
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  }
+
+
   return (
     <div className="register-form main">
-      <div  className="registration">
+      <div className="registration">
         <form>
           <h1>Registration</h1>
-          <input type="text" className="name" name="myName" placeholder="Enter your first name..."/>
-          <input type="text" className="name" name="myLastMame" placeholder="Enter your last name..."/>
-          <input type="email" className="name" name="myEmail" placeholder="Enter your email..."/>
-          <input type="password" className="name" name="myNPassword" placeholder="Enter your password..."/>
+          {(firstNameDirty && firstNameError) && <div className="alert" style={{color: "red"}}>{firstNameError}</div>}
+          <input onChange={e => firstNameHandler(e)} onBlur={e => blurHandler(e)} value={firstName} type="text"
+                 className="name" name="firstName"
+                 placeholder="Enter your first name..."/>
+
+          {(lastNameDirty && lastNameError) && <div className="alert" style={{color: "red"}}>{lastNameError}</div>}
+          <input onChange={e => lastNameHandler(e)} onBlur={e => blurHandler(e)} value={lastName} type="text"
+                 className="name" name="lastName"
+                 placeholder="Enter your last name..."/>
+
+          {(emailDirty && emailError) && <div className="alert" style={{color: "red"}}>{emailError}</div>}
+          <input onChange={e => emailHandler(e)} onBlur={e => blurHandler(e)} value={email} type="email"
+                 className="name" name="email"
+                 placeholder="Enter your email..."/>
+
+          {(passwordDirty && passwordError) && <div className="alert" style={{color: "red"}}>{passwordError}</div>}
+          <input onChange={e => passwordHandler(e)} onBlur={e => blurHandler(e)} value={password} type="password"
+                 className="name" name="password"
+                 placeholder="Enter your password..."/>
+
           <button className="registration-btn">Confirm</button>
           <button className="registration-back-btn">Back</button>
         </form>
