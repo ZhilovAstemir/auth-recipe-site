3a19b6fa8ec5895f9a734099719ab40c2fa7f503
Astemir Bekov <artisticheski@gmail.com>
2022-08-16 20:18:28 +0300

    done with styles

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,18 +11,18 @@ const LoginPage = () => {
       <div className="sub-main">
         <div>
           <div>
-            <h1>Login Page</h1>
+            <h1 className="register-h1">Login Page</h1>
             <div>
-              <input type="text" placeholder="user email" className="name"/>
+              <input type="text" placeholder="user email" className="name register-input"/>
             </div>
             <div className="second-input">
-              <input type="password" placeholder="user password" className="name"/>
+              <input type="password" placeholder="user password" className="name register-input"/>
             </div>
             <div className="login-btn">
-              <button className="btn btn-primary m-2">Login</button>
+              <button className="btn btn-primary m-2 login-btn">Login</button>
             </div>
             <p className="link">
-              or<Link to="/signin"> Sign up</Link>
+              or<Link to="/signin" className="login-link"> Sign up</Link>
             </p>
           </div>
         </div>
